fe889736326f3ecf6236af5b2023345b8d83e346
Celina <celina@systemdeMacBook-Pro-2.local>
2018-10-15 14:54:16 +0800

    Update

--- Functions homework 2/module1.py
+++
@@ -1,6 +0,0 @@
-var1 = "In Module 1"
-
-def say_hello (name):
-    print ("Hello",name)
-
-say_hello("Aria")
---
+++ nested loops.py
@@ -0,0 +1,4 @@
+for i in range (0, 5):
+    for n in range (0, 10):
+        print ("*", end = " ")
+    
